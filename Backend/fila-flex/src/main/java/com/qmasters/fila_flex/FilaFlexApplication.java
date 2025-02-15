571bf1b2b031d753e4a126865189f037e34b4fc1
Ala a <allan2021jose@gmail.com>
2025-02-15 12:23:06 -0300

    Primeiro commit

---
+++ Backend/fila-flex/src/main/java/com/qmasters/fila_flex/FilaFlexApplication.java
@@ -0,0 +1,13 @@
+package com.qmasters.fila_flex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaFlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaFlexApplication.class, args);
+	}
+
+}
---
+++ Backend/fila-flex/src/test/java/com/qmasters/fila_flex/FilaFlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmasters.fila_flex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaFlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
