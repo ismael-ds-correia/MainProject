18fff7576773f1df0af5766a9f608db57cba7389
Luzi.Alcielma <alcielmaluzinete@gmail.com>
2025-02-22 09:19:25 -0300

    [200~feat: adicionar registro de usuário com integração de API e banco de dados local

---
+++ Backend/fila-flex/src/main/java/com/qmasters/fila_flex/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.qmasters.fila_flex.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+     @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+    
+    
+
