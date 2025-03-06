f86e843711135d247e9d442e6fd107c1fdb09a07
Leite | Vinicius Leite Costa <viniciusleitecosta@gmail.com>
2025-03-06 11:23:43 -0300

    refactor: separar templates e remover componentes desnecessários
    - Separação dos templates HTML dos componentes para uma estrutura mais modular e organizada.
    - Removidos os templates inline dos arquivos .component.ts, referenciando arquivos .html externos.
    - Removidos o componente queue-table.component, pois não é mais necessários para o estado atual do sistema.

--- Frontend/FilaFlex/src/app/home/home.component.ts
+++ Frontend/FilaFlex/src/app/home/home.component.ts
@@ -1,47 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../auth/services/auth.service';
-import { AppointmentType, AppointmentTypeService } from '../services/appointment-type.service';
-import { CommonModule, CurrencyPipe, SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe, SlicePipe],
-  template: `
-    <div class="home-container">
-      <h1>Bem-vindo à página inicial</h1>
-      <button (click)="logout()" class="logout-button">Logout</button>
-      
-      <div class="appointment-grid">
-        <div *ngFor="let appointmentType of appointmentTypes" class="appointment-card">
-          <h2>{{ appointmentType.name }}</h2>
-          <p class="description">{{ appointmentType.description | slice:0:50 }}...</p>
-          <p class="price">Preço: {{ appointmentType.price | currency:'BRL' }}</p>
-          <p class="time">Tempo estimado: {{ appointmentType.estimatedTime }} minutos</p>
-        </div>
-      </div>
-    </div>
-  `,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  appointmentTypes: AppointmentType[] = [];
+export class HomeComponent {
+  private authService = inject(AuthService);
+  appointmentTypes = [
+    { name: 'Consulta Geral', description: 'Atendimento clínico geral.', price: 150, estimatedTime: 30 },
+    { name: 'Exame de Rotina', description: 'Exames periódicos para check-up.', price: 200, estimatedTime: 45 },
+  ];
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private appointmentTypeService: AppointmentTypeService
-  ) {}
-
-  ngOnInit(): void {
-    this.appointmentTypeService.getAppointmentTypes().subscribe((data) => {
-      this.appointmentTypes = data;
-    });
-  }
-
-  logout(): void {
+  logout() {
     this.authService.logout();
-    this.router.navigate(['/login']);
   }
 }
