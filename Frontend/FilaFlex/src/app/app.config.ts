615dddb9bf24e2bd2cf206ffa093f6e8ac176a2f
Leite | Vinicius Leite Costa <viniciusleitecosta@gmail.com>
2025-03-10 08:41:12 -0300

    Refatoração: Separação das rotas em app.routes.ts e configuração em app.config.ts

--- Frontend/FilaFlex/src/app/app.config.ts
+++ Frontend/FilaFlex/src/app/app.config.ts
@@ -1,17 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { Routes, provideRouter } from '@angular/router';
-import { authGuard, adminGuard } from './auth/guards/auth.guard';
+import { provideRouter } from '@angular/router';
 import routes from './app.routes';
-
-const appRoutes: Routes = [
-  { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
-  { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
-  { path: 'ServiceManagement', loadComponent: () => import('./service-management/service-management.component').then(m => m.ServiceManagementComponent), canActivate: [adminGuard] }
-];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes)
+    provideRouter(routes)
   ]
 };
