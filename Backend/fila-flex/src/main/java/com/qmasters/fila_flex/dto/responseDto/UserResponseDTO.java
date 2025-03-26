d5167f9ce70a60062c50cd8f4169ca63f914082c
Ala a <allan2021jose@gmail.com>
2025-03-26 03:02:25 -0300

    Resolvido issue de QueueService e talvez o de User

---
+++ Backend/fila-flex/src/main/java/com/qmasters/fila_flex/dto/responseDto/UserResponseDTO.java
@@ -0,0 +1,67 @@
+package com.qmasters.fila_flex.dto.responseDto;
+
+import java.util.List;
+
+import com.qmasters.fila_flex.model.Appointment;
+import com.qmasters.fila_flex.model.User;
+
+public class UserResponseDTO {
+    private Long id;
+    private String email;
+    private String name;
+    private String role;
+    private List<Appointment> appointments;
+
+    public UserResponseDTO() {
+    }
+
+    public UserResponseDTO(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.name = user.getName();
+        this.role = user.getRole().toString();
+        this.appointments = user.getAppointments();
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+}
