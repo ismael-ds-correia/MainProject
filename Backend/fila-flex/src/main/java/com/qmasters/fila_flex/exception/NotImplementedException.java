e85e42ac1d9ae4adb7dbd95c03113055cbc7a84a
Ala a <allan2021jose@gmail.com>
2025-03-30 12:45:37 -0300

    Tentando resolver Issue de ApplicationTests

---
+++ Backend/fila-flex/src/main/java/com/qmasters/fila_flex/exception/NotImplementedException.java
@@ -0,0 +1,8 @@
+package com.qmasters.fila_flex.exception;
+
+public class NotImplementedException extends RuntimeException {
+    public NotImplementedException(String message) {
+        super(message);
+    }
+    
+}
