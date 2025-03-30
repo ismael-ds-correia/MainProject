18c964f8cc5f7afdc79305964df56d635af076ee
Ala a <allan2021jose@gmail.com>
2025-03-30 14:02:34 -0300

    adicionado teste besta em ApplicationTest para resolver Issue

--- Backend/fila-flex/src/test/java/com/qmasters/fila_flex/FilaFlexApplicationTests.java
+++ Backend/fila-flex/src/test/java/com/qmasters/fila_flex/FilaFlexApplicationTests.java
@@ -1,15 +1,15 @@
 package com.qmasters.fila_flex;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.qmasters.fila_flex.exception.NotImplementedException;
 
 @SpringBootTest
 class FilaFlexApplicationTests {
 
 	@Test
 	void contextLoads() {
-        throw new NotImplementedException("Test not implemented yet");
+		assertTrue(true);
 	}
 }
